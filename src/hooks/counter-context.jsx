6362de17ff3cfa93c769f98627a25e3a28f6198f
Ring-wdr <akswnd55@gmail.com>
2022-10-29 11:01:09 +0900

    20221029 react push before refactoring

--- src/hooks/counter-context.jsx
+++ src/hooks/counter-context.jsx
@@ -1,17 +1,33 @@
-import { useState, createContext, useContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 // import { flushSync } from 'react-dom';
 
 const CountContext = createContext();
+
+const reducer = (count, action) => {
+  switch (action.type) {
+    case 'plus':
+      return count + 1;
+    case 'minus':
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
 export const CountProvider = ({ children }) => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+  // const [count, ]
   const plusCount = () => {
     // flushSync(() =>
-    setCount((count) => count + 1);
+    // setCount((count) => count + 1);
+    dispatch({ type: 'plus' });
     // );
   };
   const minusCount = () => {
     // flushSync(() =>
-    setCount((count) => count - 1);
+    // setCount((count) => count - 1);
+    dispatch({ type: 'minus' });
     // );
   };
   return (
--- src/hooks/session-context.jsx
+++ src/hooks/session-context.jsx
@@ -1,59 +1,67 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useReducer,
+  // useState,
+} from 'react';
 import { useFetch } from './fetch-hook';
-// import SampleSession from '../../public/data/sample.json';
-// const SampleSession = {
-//   loginUser: { id: 1, name: 'Hong' },
-//   cart: [
-//     { id: 100, name: '라면', price: 3000 },
-//     { id: 101, name: '컵라면', price: 2000 },
-//     { id: 200, name: '파', price: 5000 },
-//   ],
-// };
 
 const SAMPLE_URL = '/data/sample.json';
+
+const reducer = (session, action) => {
+  switch (action.type) {
+    case 'set':
+      console.log(action.payload);
+      return { ...action.payload };
+    case 'login':
+      return {
+        ...session,
+        loginUser: action.payload,
+      };
+    case 'logout':
+      return { ...session, loginUser: null };
+    case 'addCartItem':
+      session.cart.push(action.payload.item);
+      return { ...session };
+    case 'removeCartItem':
+      return {
+        ...session,
+        cart: session.cart.filter((item) => item.id !== action.payload.itemId),
+      };
+    default:
+      return session;
+  }
+};
 
 export const cachedFetch = {};
 export const SessionContext = createContext();
 export const SessionProvider = ({ children }) => {
-  const [session, setSession] = useState({ loginUser: {}, cart: [] });
+  // const [session, setSession] = useState({ loginUser: {}, cart: [] });
+  const [session, dispatch] = useReducer(reducer, {});
 
   const data = useFetch(SAMPLE_URL);
 
   useEffect(() => {
-    // console.log('dddddddddd>>', data);
-    data && setSession(data);
+    data && dispatch({ type: 'set', payload: data });
   }, [data]);
 
   const login = (id, name) => {
     console.log('App.login!!');
-    setSession({ ...session, loginUser: { id, name } });
+    dispatch({ type: 'login', payload: { id, name } });
     console.log('App.session>>>', session);
   };
   const logout = () => {
     console.log('App.logout!!');
-    // session.loginUser = null;
-    setSession({ ...session, loginUser: null });
+    dispatch({ type: 'logout' });
     console.log('App.session>>>', session);
   };
   const addCartItem = (item) => {
-    session.cart.push(item);
-    setSession({ ...session });
-    // setSession({
-    //   ...session,
-    //   cart: [...session.cart, item],
-    // });
+    dispatch({ type: 'addCartItem', payload: { item } });
   };
   const removeCartItem = (itemId) => {
-    setSession({
-      ...session,
-      cart: session.cart.filter((item) => item.id !== itemId),
-    });
+    dispatch({ type: 'removeCartItem', payload: { itemId } });
   };
-
-  // useEffect(() => {
-  // const data = useFetchByCache(SAMPLE_URL);
-  // setSession(data);
-  // }, []);
 
   return (
     <SessionContext.Provider
